795ec92041be2b360a24117e26e31890fae77c2c
Suresh <ponsuresh@live.dk>
2018-04-20 10:16:12 -0700

    Testing ...

--- WebAppTest/WebAppTest/Startup.cs
+++ WebAppTest/WebAppTest/Startup.cs
@@ -16,6 +16,7 @@ namespace WebAppTest
             Configuration = configuration;
         }
 
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
